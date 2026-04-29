ecffc10e12a95ddd804ab25ef8ee1a02ebfcd525
zmanjith <zmanjith@gmail.com>
2026-04-29 06:09:16 -0500

    Version 3 updated

--- app/backend/server.js
+++ app/backend/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify({
-    message: "Hello from Hybrid DevSecOps Platform 🚀 : Version 2"
+    message: "Hello from Hybrid DevSecOps Platform 🚀 : Version 3"
   }));
 });
 
